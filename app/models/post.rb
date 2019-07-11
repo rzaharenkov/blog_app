a10dd98ab1762d3d01809bd207612800e532d719
Raman Zakharenkau <raman@amitree.com>
2019-07-11 10:52:44 -0500

    Add caching examples.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,36 @@
+require 'compacter'
+
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
+
+  class << self
+    def cached
+      Rails.cache.fetch('posts:all') do
+        Post.includes(:comments).to_a
+      end
+    end
+
+    def cached_compact
+      compacter = Rails.cache.fetch('posts:all:cached') do
+        Compacter.new(Post.includes(:comments).to_a)
+      end
+
+      compacter&.data
+    end
+
+    def comments_stats
+      compacter = Rails.cache.fetch('posts:comments_stats') do
+        data = Post.includes(:comments).each_with_object([]) do |post, memo|
+          memo << {
+            post: post,
+            comments_count: post.comments.size
+          }
+        end
+
+        Compacter.new(data)
+      end
+
+      compacter&.data
+    end
+  end
 end
